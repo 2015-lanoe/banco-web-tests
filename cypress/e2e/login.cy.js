54af1e9f21f513b6b4066f4a7dadd465cb8efbeb
Ngfernandes <noeli.garcia.63@gmail.com>
2025-08-07 18:08:28 -0300

    Adicionando screenshots

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,6 +2,7 @@ describe('login', () => {
   beforeEach(() => {
      // Arrange
     cy.visit('http://localhost:4000')
+    cy.screenshot('apos-visitar-pagina')
 
   })
   
@@ -9,8 +10,10 @@ describe('login', () => {
     // Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
+    cy.screenshot('apos-preencher-dados-validos')
     //cy.get('#login-section > .btn').click()
     cy.contains('button', 'Entrar').click()
+    cy.screenshot('apos-clicar-no-botao-entrar')
 
     // Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
